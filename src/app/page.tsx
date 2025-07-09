b473639285f9ce0936703accd278e4a597ba9621
Omar Reda <omarredaelsayedmohamed@gmail.com>
2025-07-09 18:16:31 +0300

    fix: transfered the chat page content to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,147 @@
-import Image from "next/image";
+"use client";
+import { NavigationBar } from "@/components/NavBar";
+import { Footer } from "@/components/Footer";
+import { Message } from "@/components/Message";
+import { useEffect, useRef, useState } from "react";
 
-export default function Home() {
+type Message = {
+  content: string;
+  role: string;
+  timestamp: Date;
+};
+
+const HomePage = () => {
+  const suggestedQuestions: string[] = [
+    "What’s on your mind today?",
+    "How’s work been lately?",
+    "Need support with something emotional at work?",
+  ];
+
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (!message.trim()) return;
+    const userMessage = message;
+    setMessages((prev) => [
+      ...prev,
+      { content: message.trim(), role: "user", timestamp: new Date() },
+    ]);
+    setMessage("");
+
+    const res = await fetch("/api/assistant", {
+      method: "POST",
+      body: JSON.stringify({ message: userMessage }),
+    });
+    const data = await res.json();
+    const reply = data.reply;
+
+    setMessages((prev) => [
+      ...prev,
+      { content: reply, role: "assistant", timestamp: new Date() },
+    ]);
+  };
+
+  const getLastUserMessageIndex = () => {
+    return messages
+      .map((m, i) => ({ role: m.role, i }))
+      .filter((m) => m.role === "user")
+      .at(-1)?.i;
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <NavigationBar messages={messages} />
+      <div className="sm:px-12  md:px-40">
+        <div className="text-center py-8">
+          <h1 className="text-[#121516] text-[1.5rem] md:text-[2rem] font-bold pb-3 pt-5">
+            Your Emotional Intelligence Coach
+          </h1>
+          <h5>Ask questions, get support, grow professionally</h5>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <h5 className="text-[#121516] text-lg font-semibold px-4 pb-2 pt-4">
+            Suggested Questions
+          </h5>
+          <div className="flex gap-3 p-3 flex-wrap pr-4">
+            {suggestedQuestions.map((question, index) => (
+              <div
+                key={index}
+                onClick={() => {
+                  setMessage(question);
+                  inputRef.current?.focus();
+                }}
+                className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-full bg-[#F1F3F4] pl-4 pr-4 cursor-pointer"
+              >
+                <p className="text=[#121516] text-sm font-medium">{question}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          {messages.map((message, index) => {
+            return (
+              <div
+                key={index}
+                ref={
+                  index === getLastUserMessageIndex() ? bottomRef : undefined
+                }
+              >
+                <Message
+                  content={message.content}
+                  role={message.role}
+                  timestamp={message.timestamp}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="px-4 py-3 fixed bottom-0 w-full left-0 bg-white">
+          <form className="sm:px-12  md:px-40 py-4" onSubmit={handleSubmit}>
+            <label className="flex flex-col min-w-40 h-12 w-full">
+              <div className="flex w-full flex-1 items-stretch rounded-xl h-full">
+                <div
+                  className="text-[#6a7981] flex border-none bg-[#f1f3f4] items-center justify-center pl-4 rounded-l-xl border-r-0"
+                  data-icon="MagnifyingGlass"
+                  data-size="24px"
+                  data-weight="regular"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24px"
+                    height="24px"
+                    fill="currentColor"
+                    viewBox="0 0 256 256"
+                  >
+                    <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
+                  </svg>
+                </div>
+                <input
+                  ref={inputRef}
+                  placeholder="Ask me anything..."
+                  className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#121516] focus:outline-0 focus:ring-0 border-none bg-[#F1F3F4] focus:border-none h-full placeholder:text-[#6a7981] px-4 rounded-l-none border-l-0 pl-2 text-base font-normal leading-normal"
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                />
+              </div>
+            </label>
+          </form>
+        </div>
+      </div>
+      {messages.length !== 0 && <div className="h-120"></div>}
+      <Footer />
+      <div className="h-80"></div>
     </div>
   );
-}
+};
+
+export default HomePage;
