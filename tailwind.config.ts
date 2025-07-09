2d89cb5e1f7cd15d4596bf86d51fc3869cf056b2
Omar Reda <omarredaelsayedmohamed@gmail.com>
2025-07-09 18:56:46 +0300

    fix:solved the dark mode switch issue

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: false, // optional: controls how dark mode is applied
+  plugins: [],
+};
