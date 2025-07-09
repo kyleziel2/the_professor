46c508de8cf4b134efca9c7cd9eab04915d5e5b6
Omar Reda <omarredaelsayedmohamed@gmail.com>
2025-07-09 18:24:53 +0300

    fix: company name mistake

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer className="flex justify-center">
       <div className="flex max-w-[960px] flex-1 flex-col">
         <p className="text-[#6a7981] text-sm font-normal leading-normal pb-3 pt-1 px-4 text-center">
-          Powered by Ravi Agency
+          Powered by Revi Agency
         </p>
         <footer className="flex flex-col gap-6 px-5 py-10 text-center @container">
           <div className="flex flex-wrap items-center justify-center gap-6 @[480px]:flex-row @[480px]:justify-around">
