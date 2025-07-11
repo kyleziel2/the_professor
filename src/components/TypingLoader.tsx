3968f61a68f8b37023b8b5a2ce6545aa0308d64c
Omar Reda <omarredaelsayedmohamed@gmail.com>
2025-07-11 15:54:17 +0300

    fix: added soe padding for typing indicator

--- src/components/TypingLoader.tsx
+++ src/components/TypingLoader.tsx
@@ -1,6 +1,6 @@
 export const TypingIndicator = () => {
   return (
-    <div className="inline-flex items-center bg-gray-200 inline gap-1 px-3 py-2 rounded-full">
+    <div className="inline-flex items-center bg-gray-200 inline gap-1 m-4 px-3 py-2 rounded-full">
       <div className="h-2 w-2 bg-gray-400 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
       <div className="h-2 w-2 bg-gray-400 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
       <div className="h-2 w-2 bg-gray-400 rounded-full animate-bounce"></div>
